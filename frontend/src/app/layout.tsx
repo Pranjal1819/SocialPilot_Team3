07ff8051df7b1f17a785a482a3b2ecbe4341ebd4
Namratha <sanapalanamratha79@gmail.com>
2026-07-15 22:56:37 +0530

    Fix UI after merge

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,12 +29,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-            <AuthProvider>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
-      </AuthProvider>
-      </body>
+  <AuthProvider>
+    {children}
+  </AuthProvider>
+</body>
     </html>
   );
 }
